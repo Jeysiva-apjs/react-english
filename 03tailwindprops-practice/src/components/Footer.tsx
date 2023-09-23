21b8d32455e654523c3590f1a4076299f9ae969a
Jeysiva-apjs <jeysiva.apjs@gmail.com>
2023-09-24 00:37:02 +0530

    updated

---
+++ 03tailwindprops-practice/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p className="text-white-lg bg-gray-200 p-5">copyright@jey</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ 03tailwindprops-practice/src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-green-500 p-3 rounded-md">
+        Vite with tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
